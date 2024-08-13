c281ad8a0bed4f220275d5d331cae2ae453cf2e9
Jorrit Poelen <jhpoelen+git@jhpoelen.nl>
2024-08-13 14:40:19 -0500

    add three titles to work towards Zenodo deployment

--- track-pdfs.sh
+++ track-pdfs.sh
@@ -12,7 +12,7 @@ title=${1:-Revue suisse de zoologie}
 >&2 echo "track and package BHL container with title [${title}]"
 
 track_part_pdfs() {
-  preston track -f <(preston ls\
+  preston track --algo md5 -f <(preston ls\
    | grep "part.txt"\
    | grep hasVersion\
    | head -1\
@@ -20,8 +20,7 @@ track_part_pdfs() {
    | mlr --tsvlite filter -s title="${title}" '$ContainerTitle == @title'\
    | mlr --tsvlite cut -f PartID\
    | sed 's+^+https://www.biodiversitylibrary.org/partpdf/+g'\
-   | tail -n+2\
-   | head)
+   | tail -n+2)
 }
 
 track_part_pdfs
